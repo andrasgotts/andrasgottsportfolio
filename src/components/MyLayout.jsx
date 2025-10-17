d560b35f95eb5be7622d475cca715f7f83d7988d
andrasgotts <andras.gotts@outlook.com>
2025-10-17 11:28:07 +0100

    change svg to code

--- src/components/MyLayout.jsx
+++ src/components/MyLayout.jsx
@@ -48,11 +48,33 @@ function MyLayout() {
               <div className="card bg-[#0d0f12]">
                 <figure>
                   <span className="size-25">
-                    <img
-                      src="andrasgottsportfolio/icons/c.svg"
-                      alt="triangle with all three sides equal"
-                      className="text-white"
-                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      style={{
+                        "--darkreader-inline-fill":
+                          "var(--darkreader-background-000000, #000000)",
+                      }}
+                      viewBox="-2.4 -2.4 28.8 28.8"
+                    >
+                      <path
+                        id="SVGRepo_bgCarrier"
+                        fill="#fff"
+                        strokeWidth="0"
+                        d="M9.166.33a2.25 2.25 0 0 0-2.332 0l-5.25 3.182A2.25 2.25 0 0 0 .5 5.436v5.128a2.25 2.25 0 0 0 1.084 1.924l5.25 3.182a2.25 2.25 0 0 0 2.332 0l5.25-3.182a2.25 2.25 0 0 0 1.084-1.924V5.436a2.25 2.25 0 0 0-1.084-1.924z"
+                        style={{
+                          "--darkreader-inline-fill":
+                            "var(--darkreader-background-ffffff, #181a1b)",
+                        }}
+                        transform="matrix(1.8 0 0 1.8 -2.4 -2.4)"
+                      ></path>
+                      <g id="SVGRepo_iconCarrier">
+                        <path d="M6.078 12A5.93 5.93 0 0 1 12 6.078a5.95 5.95 0 0 1 5.129 2.959l-1.499.867 5.027-2.902a1.6 1.6 0 0 0-.543-.575l-7.345-4.24c-.424-.245-1.116-.245-1.539 0l-7.345 4.24c-.422.244-.768.844-.768 1.333v8.48c0 .245.086.517.226.758l3.529-2.038A5.9 5.9 0 0 1 6.078 12"></path>
+                        <path d="M14.564 10.519A2.97 2.97 0 0 0 12 9.039a2.964 2.964 0 0 0-2.564 4.441L12 12z"></path>
+                        <path d="m20.657 7.002-5.046 2.913-1.046.605-.001-.001L12 12l-2.563 1.48A2.96 2.96 0 0 0 12 14.961a2.97 2.97 0 0 0 2.565-1.481l2.563 1.483a5.94 5.94 0 0 1-5.129 2.959 5.93 5.93 0 0 1-5.128-2.962l-3.529 2.038c.14.242.332.453.543.575l7.345 4.24c.423.245 1.115.245 1.539 0l7.345-4.24c.211-.122.403-.333.543-.575s.226-.513.226-.758V7.76c0-.245-.086-.517-.226-.758m-2.735 5.327h-.658v.658h-.657v-.658h-.658v-.658h.658v-.658h.657v.658h.658zm2.468 0h-.658v.658h-.658v-.658h-.657v-.658h.657v-.658h.658v.658h.658z"></path>
+                      </g>
+                    </svg>
                   </span>
                 </figure>
               </div>
@@ -86,11 +108,21 @@ function MyLayout() {
               <div className="card bg-[#0d0f12]">
                 <figure>
                   <span className="size-25">
-                    <img
-                      src="andrasgottsportfolio/icons/pytorch.svg"
-                      alt="triangle with all three sides equal"
-                      className="text-white"
-                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="40"
+                      height="40"
+                      fill="#fff"
+                      style={{
+                        "--darkreader-inline-fill":
+                          "var(--darkreader-background-ffffff, #181a1b)",
+                      }}
+                      viewBox="0 0 32 32"
+                    >
+                      <g id="SVGRepo_iconCarrier">
+                        <path d="M20.465 5.877a.687.687 0 1 1 0 .002zm-4.459-4.873L7.192 9.818a12.35 12.35 0 0 0-3.654 8.786c0 6.843 5.547 12.39 12.39 12.39 3.434 0 6.541-1.397 8.785-3.653l.001-.001a12.2 12.2 0 0 0 3.748-8.814c0-3.408-1.393-6.49-3.641-8.709l-.001-.001-2.179 2.178a9.312 9.312 0 0 1-6.585 15.896 9.312 9.312 0 0 1-6.585-15.896l5.807-5.806.727-.831z"></path>
+                      </g>
+                    </svg>
                   </span>
                 </figure>
               </div>
